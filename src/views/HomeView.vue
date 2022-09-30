09eda2bff04c74f20230487363697880d1c65568
Jerydeak <jerydeak@qq.com>
2022-10-01 01:54:18 +0800

    Jerydeak: init

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,44 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <div class="container">
+      <div class="area" style="background-color: gray;">Helasd asd asd as das lo</div>
+      <div class="area" style="background-color: gold;">Hasd asd asd asd ad sad@</div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+  // @ is an alias to /src
+  // import HelloWorld from '@/components/HelloWorld.vue'
 
-export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
+  export default {
+    name: 'HomeView',
+    components: {
+      // HelloWorld
+    }
   }
-}
 </script>
+
+<style>
+  /* .container {
+    display: flex;
+    padding: 20px;
+    align-items: center;
+    justify-content: center;
+  } */
+
+  @media(min-width: 500px) {
+    .container {
+      display: flex;
+      padding: 20px;
+      align-items: center;
+      justify-content: center;
+    }
+  }
+
+  .area {
+    padding: 10px;
+  }
+</style>
